973ec846a5b7c4550c2155b194ccba76fb9b49b0
alkadabna <168734250+alkadabna@users.noreply.github.com>
2024-05-22 16:18:09 +0200

    final changes

--- jour2/job6.php
+++ jour2/job6.php
@@ -2,12 +2,10 @@
 $largeur = 30;
 $hauteur = 10;
 
-// Boucle pour afficher les lignes du rectangle
 for ($i = 0; $i < $hauteur; $i++) {
-    // Boucle pour afficher les colonnes du rectangle
     for ($j = 0; $j < $largeur; $j++) {
-        echo "¤"; // Affichage du caractère "*" pour représenter le rectangle
+        echo "¤"; 
     }
-    echo "<br />"; // Retour à la ligne après chaque ligne du rectangle
+    echo "<br />"; 
 }
 ?>
---
+++ jour2/job7.php
@@ -0,0 +1,10 @@
+<?php
+$hauteur = 10;
+for ($i = 1; $i <= $hauteur; $i++) {
+    
+    for ($j = 1; $j <= $i; $j++) {
+        echo "p"; 
+    }
+    echo "<br />";
+}
+    ?>
